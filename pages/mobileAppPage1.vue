581bcbf69c19e06e5c6b7fe89b9a79ebb96cf7fb
w3WebTech <164163622+w3WebTech@users.noreply.github.com>
2024-09-23 19:34:51 +0530

    fix

--- pages/mobileAppPage1.vue
+++ pages/mobileAppPage1.vue
@@ -191,7 +191,7 @@
             </div>
             <div class="flex justify-between mb-2">
               <label class="inline-flex items-center mb-5 cursor-pointer">
-                <span class="mr-3 py-1 text-sm font-medium text-gray-700 dark:text-gray-300">yes</span>
+                <span class="mr-3 py-1 text-sm font-medium text-gray-700 dark:text-gray-300">no</span>
                 <input
                   type="checkbox"
                   :value="this.questions[currentIndex].isMessageMandatory"
@@ -208,7 +208,7 @@
                 <div
                   class="relative w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
                 ></div>
-                <span class="ms-3 text-sm font-medium text-gray-700 dark:text-gray-300">no</span>
+                <span class="ms-3 text-sm font-medium text-gray-700 dark:text-gray-300">yes</span>
               </label>
             </div>
             <!-- rest of the question content -->
