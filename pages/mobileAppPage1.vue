b888936a80b3f226a247d9a43a149ed1b520219b
w3WebTech <164163622+w3WebTech@users.noreply.github.com>
2025-01-31 13:22:08 +0530

    fix

--- pages/mobileAppPage1.vue
+++ pages/mobileAppPage1.vue
@@ -328,16 +328,20 @@
       <div class="modal-content">
         <!-- Close Button -->
         <span class="close" @click="showConfirmationModal = false">&times;</span>
+<div class="flex justify-center items0-center text-center ">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="green" class="h-20 w-20">
+  <path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+</svg>
+</div>
+   
 
-        <!-- Green Tick Mark -->
-        <i class="pi pi-check-circle text-6xl text-green-500 mb-4"></i>
 
         <!-- Thank You Message -->
         <p class="font-medium text-xl text-gray-800">Thank you for your response!</p>
 
         <!-- Optional Button (like OK) -->
-        <button @click="showConfirmationModal = false" class="mt-4 px-6 py-2 bg-green-500 text-white rounded hover:bg-green-600">
-          OK
+        <button @click="showConfirmationModal = false" class="mt-4 px-6 py-2 bg-blue-500 text-white rounded hover:bg-green-600">
+          OK,Thanks
         </button>
       </div>
 </div>
